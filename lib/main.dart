aad73c4d1e0e72481f164035c661eb514b1ae620
Dell <eheba0302@gmail.com>
2023-08-25 17:21:57 +0300

    intial app

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+    ));
+  }
+}
